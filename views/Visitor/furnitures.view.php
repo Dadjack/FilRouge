37a7107f96e024767738470fccd86ae36ba9590c
Dadjack <88081348+Dadjack@users.noreply.github.com>
2022-06-26 22:06:48 +0200

    derniere modif

--- views/Visitor/furnitures.view.php
+++ views/Visitor/furnitures.view.php
@@ -1,21 +1,23 @@
 <h1>MEUBLES DECO</h1>
 <table class="table text-center">
         <tr class="table-dark">
-                <th>Image Du Produit</th>
-                <th>Nom du produit</th>
-                <th>Description Du Produit</th>
-                <th>Quantité Du Produit</th>
-                <th>Prix Du Produit</th>
+                <th>Image</th>
+                <th>Nom</th>
+                <th>Description</th>
+                <th>Quantité</th>
+                <th>Prix</th>
         </tr>
         <?php
                 for($i=0; $i < count($products);$i++) :
         ?>
-        <tr>
-                <td class="align-middle"><a href="<?= URL ?>products/p/<?= $products[$i]->getIdProduct(); ?>"><img src="<?= URL ?>public/images/ImgM/<?= $products[$i]->getProductImage(); ?>" width="400px;"></a></td>
+        <tr class="container">
+                <td class="align-middle border border-dark"><a href="<?= URL ?>products/p/<?= $products[$i]->getIdProduct(); ?>"><img src="<?= URL ?>public/images/ImgM/<?= $products[$i]->getProductImage(); ?>" width="400px;"></a></td>
+                <div class="container border border-dark">
                 <td class="align-middle"><?= $products[$i]->getProductName(); ?></td>
                 <td class="align-middle"><?= $products[$i]->getProductDescription();  ?></a></td>
                 <td class="align-middle"><?= $products[$i]->getProductQuantity(); ?></td>
-                <td class="align-middle"><?= $products[$i]->getProductPrice()?></td>      
+                <td class="align-middle"><?= $products[$i]->getProductPrice()?></td> 
+                </div>     
         </tr>
                 <!--<> JE FERME MA BOUCLE FOR <>--> 
         <?php endfor; ?>
